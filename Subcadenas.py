d77846de5b90911944d7a2577a411aebaa71b0cf
fever <122796754+luisave1@users.noreply.github.com>
2023-12-26 11:45:10 -0500

    Update Subcadenas.py

--- Subcadenas.py
+++ Subcadenas.py
@@ -1,1 +1,13 @@
+#Pedimos una cadena 
+cadena = input('Dame una cadena: ')
+#Pedimos un tamaño de la subcadena
+n = int(input('Dame un nunmero: '))
+
+#resolvemos todas las subadenas de la cadena de tamaño n
+for k in range(len(cadena)-(n-1)):
+    subcadena = ''
+    for i in range(k + n):
+        subcadena += cadena[i]
+    #mostramos las subcadenas que queremos
+    print(subcadena[k:k+n])
 
